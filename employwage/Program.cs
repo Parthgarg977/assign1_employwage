0f99cd02b065fabfb083937f13ef845cbdf77f91
parthgarg977 <parthgarg977@gmail.com>
2022-08-02 07:27:48 +0530

    1st UC4 commit

--- employwage/Program.cs
+++ employwage/Program.cs
@@ -20,22 +20,35 @@ namespace employwage
                 Console.WriteLine("Employee is absent");
             }
             //UC2 UC3
-            int Is_FullTime = 1;
-            int Is_PartTime = 2;
+           const int Is_FullTime = 1;
+          const  int Is_PartTime = 2;
             int RatePerHour = 20;
             int empHrs;
             int empcheck = random.Next(0, 3);
-            if (empcheck == Is_FullTime)
+            /* if (empcheck == Is_FullTime)
+             {
+                 empHrs = 8;
+             }
+             else if (empcheck == Is_PartTime)
+             {
+                 empHrs = 4;
+             }
+             else
+             {
+                 empHrs = 0;
+             }*/
+            switch (empcheck)
             {
-                empHrs = 8;
-            }
-            else if (empcheck == Is_PartTime)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case Is_FullTime:
+                    empHrs = 8;
+                    break;
+                    case Is_PartTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
             }
             int empWage = empHrs * RatePerHour;
             Console.WriteLine("Employee wage is {0}", empWage);
