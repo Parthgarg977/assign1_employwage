f55edcd78ecdfc24b472a9220afe7f7dcc863acd
parthgarg977 <parthgarg977@gmail.com>
2022-08-02 06:38:47 +0530

    2nd commit

--- employwage/Program.cs
+++ employwage/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace employwage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
