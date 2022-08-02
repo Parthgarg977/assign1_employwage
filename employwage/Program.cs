a2a9d899ea50f7b70650c4daf492c2c5954a1ea6
parthgarg977 <parthgarg977@gmail.com>
2022-08-02 07:09:01 +0530

    2nd UC2 commit

--- employwage/Program.cs
+++ employwage/Program.cs
@@ -7,10 +7,20 @@ namespace employwage
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int attendence = random.Next(0, 2);
+            if (attendence == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
             int Is_FullTime = 1;
             int RatePerHour = 20;
             int empHrs;
-            Random random = new Random();
             int empcheck = random.Next(0, 2);
             if (empcheck == Is_FullTime)
             {
