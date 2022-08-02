cfa007a67b49970a79e63efef5799e34df2abfa0
parthgarg977 <parthgarg977@gmail.com>
2022-08-02 06:50:19 +0530

    1st UC1 commit

--- employwage/Program.cs
+++ employwage/Program.cs
@@ -8,6 +8,16 @@ namespace employwage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int attendence = random.Next(0, 2);
+            if (attendence == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
